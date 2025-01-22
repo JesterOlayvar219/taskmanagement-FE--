df16580becad7f3f26af228a833f36d4156e52c3
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2025-01-22 23:51:28 +0900

    UI update

--- components/todos/AllTasks.tsx
+++ components/todos/AllTasks.tsx
@@ -5,28 +5,27 @@ interface AllTasksProps {
   taskList: React.ReactNode;
 }
 
-const AllTasks:React.FC<AllTasksProps> = ({ taskList }) => {
+const AllTasks: React.FC<AllTasksProps> = ({ taskList }) => {
   return (
     <TodoContext.Provider value={true}>
-      <div className="flex flex-col w-60 border-t-4 border-sky-500 rounded-md bg-sky-400 p-2 px-2">
-        <div className="mb-5 flex flex-row items-center gap-2">
-          {/* <Image src={"/assets/todos/note.png"} alt="To Do" width={15} height={15}/> */}
-          <Image
-            src="/assets/todos/note.png"
-            alt="ToDo"
-            width={15}
-            height={15}
-            style={{ width: "auto", height: "auto" }}
-            suppressHydrationWarning={true}
-          />
-
-          <h1 className="text-xl text-white">All</h1>
+      <div className="flex flex-col w-[352px] rounded-md bg-medium_black_200 p-6 px-4 space-y-4">
+        <div className="flex flex-row justify-between items-center">
+          <span className="text-sm text-gray-500 font-bold">All data(4)</span>
+          <button className="flex flex-row justify-center items-center gap-2">
+            <Image
+              src="/assets/todos/plus.png"
+              alt="plusBtn"
+              width={18}
+              height={18}
+            />
+            <span className="text-sm text-white font-bold">Add new task</span>
+          </button>
         </div>
-        <div className="max-h-[calc(48px*8)] overflow-y-auto scrollbar-thin scrollbar-thumb-sky-400 scrollbar-track-sky-600">
+        <div className="max-h-[826px] overflow-y-auto scrollbar-thin scrollbar-thumb-sky-400 scrollbar-track-sky-600 ">
           <ul
             role="list"
             aria-labelledby="list-heading"
-            className="flex flex-col gap-4"
+            className="flex flex-col gap-[14px]"
           >
             {taskList}
           </ul>
@@ -34,6 +33,6 @@ const AllTasks:React.FC<AllTasksProps> = ({ taskList }) => {
       </div>
     </TodoContext.Provider>
   );
-}
+};
 
 export default AllTasks;
