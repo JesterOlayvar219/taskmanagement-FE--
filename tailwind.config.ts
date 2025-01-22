3f46521ecd32724dfce138f6f1b21b8754cabbaf
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2025-01-22 23:51:54 +0900

    add color customize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,12 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        medium_gray_100:"#E1E8ED",
+        medium_gray_100: "#E1E8ED",
+        medium_gray_200: "#292B31",
+        medium_gray_300: "#989CAA",
+        medium_black_100: "#2A2B2F",
+        medium_black_200: "#222327",
+        medium_black_300: "#24262C",
       },
     },
   },
